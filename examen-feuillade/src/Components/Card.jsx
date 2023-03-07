d94b0872a279e6204b2688617378db994f89a0fe
Angi <angelesfeuillade@gmail.com>
2023-03-07 00:30:39 -0300

    actualizacion

--- examen-feuillade/src/Components/Card.jsx
+++ examen-feuillade/src/Components/Card.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div>Hola soy una tarjeta</div>
+    <div>Hola soy una tarjeta
+        <h3>Esto es un h3</h3>
+    </div>
+    
   )
 }
 
