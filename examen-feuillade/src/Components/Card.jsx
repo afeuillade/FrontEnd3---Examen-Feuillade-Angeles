4bbdd542fa748847b6f46cd23b4e4ce66f8e2dbe
Angi <angelesfeuillade@gmail.com>
2023-03-08 23:26:36 -0300

    Examen parcial front

--- examen-feuillade/src/Components/Card.jsx
+++ examen-feuillade/src/Components/Card.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({nombre, apellido, colorFavorito}) => {
   return (
-    <div>Hola soy una tarjeta
-        <h3>Esto es un h3</h3>
+    <div style={{flexDirection: 'column',float:'left', width: '300px', margin: '10%',padding: '10%',display:'flex', justifyContent:'space-around', border: '1px solid green', backgroundColor:'lightgrey'}}>
+    <h3>Hola {nombre} {apellido} ! </h3>
+    <h4>Tu color favorito es  {colorFavorito} </h4>
+
     </div>
     
   )
--- examen-feuillade/src/Components/Form.jsx
+++ examen-feuillade/src/Components/Form.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Card from './Card'
 
 const Form = () => {
-  return (
-    <div>
-        <Card/>
-    </div>
+    const [user,setUser] = useState({
+        nombre: '',
+        apellido: '',
+        email:'',
+        colorFavorito: ''
+    })
+    const [show,setShow] = useState(false)
+    const [err,setErr] = useState(false)
+
+    const handleSubmit = (e) =>{
+            e.preventDefault()
+            if(user.nombre.length > 3 && user.apellido.length >=6 ){
+                setShow(true)
+                setErr(false)
+            }else{
+                setShow(false)
+                setErr(true)
+            }
+    }
+
+
+    return(
+        <div>
+            <h2>Elige un color</h2>
+        <div style={{flexDirection: 'column', width: '300px', margin: '10%',padding: '10%',display:'flex', justifyContent:'space-around', border: '1px solid green', backgroundColor:'lightgrey'}}>
+            
+            <form onSubmit={handleSubmit}>
+                <input placeholder='Ingrese su nombre' type="text" value={user.nombre} onChange={(e)=> setUser({...user, nombre: e.target.value})} />
+                <input placeholder='Ingrese su apellido' type="text" value={user.apellido} onChange={(e)=> setUser({...user, apellido: e.target.value})} />
+                <input placeholder='Ingrese su e-mail' type="email" value={user.email} onChange={(e)=> setUser({...user, email: e.target.value})}/>
+                <input placeholder='Ingrese su color favorito' type="text" value={user.colorFavorito}onChange={(e)=> setUser({...user, colorFavorito: e.target.value})}/>
+
+                <button style={{color:'white', backgroundColor:'blue', width:'150px', margin: '10%',padding: '10%'}}>Enviar</button>
+            </form>
+            <br />
+            <p style={{color:'red'}}>{err && 'Por favor chequea que la información sea correcta'}</p>
+
+
+
+
+
+
+        </div>
+        {show &&  <Card nombre={user.nombre} apellido={user.apellido} colorFavorito = {user.colorFavorito}/>}
+        </div>
   )
 }
 
